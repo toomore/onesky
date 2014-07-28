20abb32f5245f58922333c882c94708d02f7dda9
Toomore Chiang <toomore0929@gmail.com>
2014-07-28 23:58:37 +0800

    Object.

--- onesky.py
+++ onesky.py
@@ -4,33 +4,44 @@ import requests
 import setting
 import time
 from pprint import pprint
+from urlparse import urljoin
 
+class Onesky(object):
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.api_path = 'https://platform.api.onesky.io/1/'
 
-def render_auth():
-    timestamp = str(int(time.time()))
-    dev_hash = hashlib.md5()
-    dev_hash.update(timestamp)
-    dev_hash.update(setting.API_SECRET)
-    return {
-            'api_key': setting.API_KEY,
-            'timestamp': timestamp,
-            'dev_hash': dev_hash.hexdigest()
-           }
+    def render_auth(self):
+        timestamp = str(int(time.time()))
+        dev_hash = hashlib.md5()
+        dev_hash.update(timestamp)
+        dev_hash.update(self.api_secret)
+        return {
+                'api_key': self.api_key,
+                'timestamp': timestamp,
+                'dev_hash': dev_hash.hexdigest()
+               }
 
-def api(api_path):
-    r = requests.get('https://platform.api.onesky.io/1/%s' % api_path,
-            params=render_auth())
-    return r.json()
+    def api_get(self, api_path, params=None):
+        if not params and isinstance(params, dict):
+            params.update(self.render_auth())
+        else:
+            params = self.render_auth()
 
-def upload_po(project_id, file):
-    params = render_auth()
-    params['file_format'] = 'GNU_PO'
-    r = requests.post('https://platform.api.onesky.io/1/projects/%s/files' % project_id,
-            params=params, files={'file': file})
-    return r.json()
+        result = requests.get(urljoin(self.api_path, api_path), params=params)
+        return result.json()
+
+    def upload_po(self, project_id, file):
+        params = render_auth()
+        params['file_format'] = 'GNU_PO'
+        r = requests.post(urljoin(self.api_path, 'projects/%s/files' % project_id),
+                params=params, files={'file': file})
+        return r.json()
 
 if __name__ == '__main__':
-    #pprint(api('project-groups'))
-    #pprint(api('project-groups/%s/projects' % setting.PROJECT_GROUP_ID))
-    with open(setting.PO_FILES, 'rb') as pof:
-        pprint(upload_po(setting.PROJECT_ID, pof))
+    onesky = Onesky(setting.API_KEY, setting.API_SECRET)
+    pprint(onesky.api_get('project-groups'))
+    pprint(onesky.api_get('project-groups/%s/projects' % setting.PROJECT_GROUP_ID))
+    #with open(setting.PO_FILES, 'rb') as pof:
+    #    pprint(upload_po(setting.PROJECT_ID, pof))
