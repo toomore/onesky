9f305b943bcceb1e47db5edf9c4f683c0ae478af
Toomore Chiang <toomore0929@gmail.com>
2015-02-07 21:37:51 +0800

    Remove unused code.

--- gettext-go/main.go
+++ gettext-go/main.go
@@ -3,51 +3,12 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	//"code.google.com/p/gettext-go/gettext/po"
 	"github.com/toomore/gettext-go/gettext/po"
 )
-
-func copyAndAdd() {
-	pofile, err := po.Load("test.po")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for i, v := range pofile.Messages {
-		fmt.Println(i, v.MsgId)
-	}
-	fmt.Println(pofile.MimeHeader)
-	n := &po.Message{
-		MsgId:  "Toomore",
-		MsgStr: "MsgToomore",
-	}
-	pofile.Messages = append(pofile.Messages, *n)
-	for i, v := range pofile.Messages {
-		fmt.Println(i, v.MsgId, v.MsgStr)
-	}
-	pofile.Save("test_result.po")
-}
-
-func createWithHeader(filename string) {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		f, _ := os.Create(filename)
-		f.Close()
-	}
-	pofile, _ := po.Load(filename)
-	header := po.Header{
-		ProjectIdVersion: "Toomore",
-	}
-	pofile.MimeHeader = header
-	pofile.Messages = append(pofile.Messages, po.Message{
-		MsgId: "Toomore", MsgStr: "MsgToomore"})
-
-	fmt.Println(header)
-	orgfilename := strings.Split(filename, ".")
-	pofile.Save(fmt.Sprintf("%s_result.%s", orgfilename[0], orgfilename[1]))
-}
 
 func createPO(filename string, csvdata [][]string, rownum int) {
 	var lang = csvdata[0][rownum]
@@ -93,7 +54,6 @@ func csvtopo(filename string) {
 }
 
 func main() {
-	//createWithHeader("test2.po")
 	csvtopo("onesky.csv")
 	//csvdata, _ := readCSV("onesky.csv")
 	//createPO("onesky.po", csvdata, 0)
